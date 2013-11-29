37ebba0f9ae18e50275fd2c5537774ddef32a72a
sindredl <sindredl@msn.com>
2013-11-29 16:54:18 +0100

    updated some stuff assignment2 - ex22

--- mystuff/ex26.py
+++ mystuff/ex26.py
@@ -14,7 +14,7 @@ def print_first_word(words):
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1)
+    word = words.pop(-1) #Closing bracked added
     print word
 
 def sort_sentence(sentence):
@@ -64,13 +64,13 @@ print "This should be Six: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans / 1000
+    jars = jelly_beans / 1000 #added slash instad of backslash
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point) #start-point from start_point
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
@@ -78,7 +78,7 @@ print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point)
+print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_point) # added closing parantheses
 
 #-----------------------------------------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------------------------------------#
@@ -93,11 +93,11 @@ sorted_words = ex25.sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-print_first_word(sorted_words)
+print_first_word(sorted_words) #removed a .
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-print sorted_words
+print sorted_words #added t to print
 
-print_first_and_last(sentence)
+print_first_and_last(sentence) #added f to first
 
 print_first_and_last_sorted(sentence)
